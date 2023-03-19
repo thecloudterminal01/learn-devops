cbc3c2feae90a5ff027786b6475b2e96dd51c5b0
thecloudterminal <the.cloud.terminal.01@gmail.com>
2023-03-19 12:00:25 +0400

    chore: update

--- home/infrastructure-as-code/terraform-gcp/taskset/task-006-modular-load-balancing-regional-load-balancer/ReadMe.md
+++ home/infrastructure-as-code/terraform-gcp/taskset/task-006-modular-load-balancing-regional-load-balancer/ReadMe.md
@@ -46,3 +46,43 @@ EXTERNAL_IP=$(terraform output | grep load_balancer_default_ip | cut -d = -f2 | 
 
 echo "http://${EXTERNAL_IP}"
 ```
+
+## Screenshots
+
+- Forwarding rules
+  - In GCP, a forwarding rule is a configuration that specifies how traffic should be directed to a load balancer. 
+    It is a key component of GCP's load balancing service, which distributes incoming traffic to a group of backend instances.
+  - A forwarding rule specifies the IP address, protocol, and ports that the load balancer should listen on. 
+    It also defines the target pool, which is a group of backend instances that the load balancer will direct traffic to. 
+  - There are different types of forwarding rules in GCP, including regional forwarding rules, global forwarding rules, 
+    and target forwarding rules. Each type is used for different load balancing scenarios, and each has its own set of configuration options.
+
+![img.png](.images/forwarding-rules.png)
+
+- Target Pools
+  - In Google Cloud Platform (GCP), a target pool is a group of virtual machine (VM) instances or internet protocol (IP) addresses that receive incoming 
+    traffic from a Google Cloud load balancer. The target pool defines the set of virtual machines or IP addresses that should receive traffic, and the 
+    load balancer routes traffic to the instances in the pool based on the load balancing algorithm configured for the load balancer. 
+  - Target pools provide a way to distribute traffic across multiple instances of a service, which can help improve availability and scalability. 
+    By grouping instances or IP addresses into a target pool, GCP can direct traffic to available instances, and take instances out of rotation as needed, 
+    based on health checks or other configurable criteria.
+
+![img.png](.images/target-pools.png)
+
+- Health checks
+  - Health checks in GCP are used to monitor the health and availability of instances or endpoints. 
+    A health check sends requests to an instance or endpoint and analyzes the response to determine whether the instance or endpoint is healthy or not. 
+  - In GCP, there are several types of health checks available, such as HTTP, HTTPS, TCP, and SSL health checks. 
+    These health checks can be used to monitor instances running on Compute Engine, instances running on Kubernetes Engine, or endpoints 
+    running on App Engine, Cloud Functions, or Cloud Run. 
+  - By using health checks, you can configure load balancers to automatically remove unhealthy instances from a load-balancing pool 
+    and redirect traffic to healthy instances. This helps to ensure that your applications are always available to users, even 
+    if some instances or endpoints fail.
+
+![img.png](.images/health-checks.png)
+
+![img.png](.images/basic-load-balancer-custom-hc-hc.png)
+
+![img.png](.images/basic-load-balancer-default-hc.png)
+
+
--- home/infrastructure-as-code/terraform-gcp/taskset/task-006-modular-load-balancing-regional-load-balancer/main.tf
+++ home/infrastructure-as-code/terraform-gcp/taskset/task-006-modular-load-balancing-regional-load-balancer/main.tf
@@ -1,6 +1,23 @@
+/*
+The "locals" block defines a variable named "health_check_port" that is set to the value of "var.health_check["port"]".
+*/
 locals {
   health_check_port = var.health_check["port"]
 }
+
+/*
+In GCP, a forwarding rule is a configuration that specifies how traffic should be directed to a load balancer.
+A forwarding rule specifies the IP address, protocol, and ports that the load balancer should listen on.
+It also defines the target pool, which is a group of backend instances that the load balancer will direct traffic to.
+
+The "google_compute_forwarding_rule" resource creates a forwarding rule to route traffic to the target pool. 
+It specifies the provider, project, name, target, load balancing scheme, port range, region, IP address, IP protocol, and labels.
+
+Created for 
+- basic-load_balancer_default
+- basic-load_balancer_custom_hc
+- basic-load_balancer_no_hc
+*/
 
 resource "google_compute_forwarding_rule" "default" {
   provider              = google-beta
@@ -15,6 +32,19 @@ resource "google_compute_forwarding_rule" "default" {
   labels                = var.labels
 }
 
+/*
+The "google_compute_target_pool" resource creates a target pool to distribute traffic across instances. 
+It specifies the project, name, region, and session affinity.
+The "health_checks" attribute is set to an empty list if "var.disable_health_check" is true, otherwise it includes a reference to the HTTP health check resource
+
+Created for
+- basic-load-balancer-default: creates healthcheck : basic-load-balancer-default-hc : Path /, port 80
+- basic-load-balancer-custom-hc : creates healthcheck : basic-load-balancer-custom-hc : Host 1.2.3.4 and path is /mypath, port 8080
+- basic-load_balancer_no_hc : "health_checks" attribute is set to an empty list : So health check will be set to NONE
+
+Target Pools are associated with healthchecks.
+
+*/
 resource "google_compute_target_pool" "default" {
   project          = var.project
   name             = var.name
@@ -24,6 +54,15 @@ resource "google_compute_target_pool" "default" {
   health_checks = var.disable_health_check ? [] : [google_compute_http_health_check.default[0].self_link]
 }
 
+/*
+The "google_compute_http_health_check" resource creates an HTTP health check to verify the health of instances. 
+It specifies the project, name, check interval, healthy threshold, timeout, unhealthy threshold, port, request path, and host.
+
+The count parameter is used to conditionally create the resource depending on the value of the disable_health_check variable. 
+If disable_health_check is set to true, the health check resource will not be created.
+
+Health check is used by a Target Pool
+*/
 resource "google_compute_http_health_check" "default" {
   count   = var.disable_health_check ? 0 : 1
   project = var.project
@@ -39,6 +78,12 @@ resource "google_compute_http_health_check" "default" {
   host         = var.health_check["host"]
 }
 
+/*
+The "google_compute_firewall" resources create firewall rules to control network traffic. 
+The first firewall ("default-lb-fw") allows traffic on the specified protocol and port, from the specified source ranges, 
+to instances with the specified target tags and service accounts. 
+
+*/
 resource "google_compute_firewall" "default-lb-fw" {
   project = var.firewall_project == "" ? var.project : var.firewall_project
   name    = "${var.name}-vm-service"
@@ -56,6 +101,11 @@ resource "google_compute_firewall" "default-lb-fw" {
   target_service_accounts = var.target_service_accounts
 }
 
+/*
+The second firewall ("default-hc-fw") allows TCP traffic on the health check port, from specified source ranges, 
+to instances with the specified target tags and service accounts. 
+The count attribute is used to conditionally create this resource based on whether health checks are disabled or not.
+*/
 resource "google_compute_firewall" "default-hc-fw" {
   count   = var.disable_health_check ? 0 : 1
   project = var.firewall_project == "" ? var.project : var.firewall_project
